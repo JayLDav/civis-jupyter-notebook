e4505509395aa79b0b748409e28b87e2e0bb0b31
JayLDav <naphikes@gmail.com>
2020-01-22 16:29:24 -0500

    Update run.sh

--- example/run.sh
+++ example/run.sh
@@ -3,4 +3,4 @@
 echo "PLATFORM_OBJECT_ID=<NOTEBOOK ID>
 CIVIS_API_KEY=${CIVIS_API_KEY}" > my.env
 
-exec docker run --rm -p 8888:8888 --env-file my.env py3
+exec docker run --rm -p 14741 --env-file my.env py3
